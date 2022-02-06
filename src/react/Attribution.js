b0f6844cf2be0e213980ace807a33b5cd424b588
martin-k-kamir <martin.k.kamir@gmail.com>
2022-02-06 16:51:32 +0100

    Update Attribution.js

--- src/react/Attribution.js
+++ src/react/Attribution.js
@@ -1,8 +1,8 @@
 export default function Attribution() {
   return (
     <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://martinkamir.com" target="_blank">Martin Kamír</a>.
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+    Coded by <a href="https://martinkamir.com" target="_blank" rel="noreferrer">Martin Kamír</a>.
     </div>
   );
 }
